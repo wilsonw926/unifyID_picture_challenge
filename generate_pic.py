88b330b50453bec89fe78a2c3025360c6b989b4f
Wilson Wang <wilsonw926@berkeley.edu>
2017-10-20 12:16:00 -0700

    constants

--- generate_pic.py
+++ generate_pic.py
@@ -1,2 +1,11 @@
 #Random Bitmap Image Coding Challenge for UnifyID
 #Author: Wilson Wang (github.com/wilsonw926)
+
+import requests
+import numpy as np
+from PIL import Image
+
+#Constants
+IMG_ROW = 128
+IMG_COL = 128
+TOTAL_PIXELS = IMG_ROW * IMG_COL
